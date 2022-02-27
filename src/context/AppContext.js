aea75451b8a247095a3aec373752db36dfd6c723
kassius <kassius@hotmail.com>
2022-02-27 23:14:49 +0200

    added loading and failure feedback for context

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -9,23 +9,40 @@ export default function Context({ children }) {
   const [tabs, setTabs] = useState([]);
   const [hasChanged, setHasChanged] = useState(true);
   const [isFailure, setIsFailure] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     if (hasChanged === true) {
+      setIsLoading(true);
+
+      let tabsLoaded = false;
+      let vehiclesLoaded = false;
+
       tabApi.getAllTabs().then((response) => {
         if (response.success === true) {
+          tabsLoaded = true;
           setTabs(response.tabs);
-          setHasChanged(false);
+          if (tabsLoaded && vehiclesLoaded) {
+            setIsFailure(false);
+            setIsLoading(false);
+          }
         } else {
           setIsFailure(true);
+          setIsLoading(false);
           setTabs([]);
         }
       });
       vehicleApi.getAllVehicles().then((response) => {
         if (response.success === true) {
+          vehiclesLoaded = true;
           setVehicles(response.vehicles);
+          if (tabsLoaded && vehiclesLoaded) {
+            setIsFailure(false);
+            setIsLoading(false);
+          }
         } else {
           setIsFailure(true);
+          setIsLoading(false);
           setVehicles([]);
         }
       });
@@ -40,6 +57,7 @@ export default function Context({ children }) {
     setTabs: setTabs,
     setHasChanged: setHasChanged,
     isFailure: isFailure,
+    isLoading: isLoading,
   };
 
   return <AppContext.Provider value={context}>{children}</AppContext.Provider>;
--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -1,12 +1,48 @@
 import "./mainLayout.scss";
 import Sidebar from "./Sidebar";
 import { Outlet } from "react-router-dom";
+import { AppContext } from "context/AppContext";
+import React, { useContext, useEffect, useState } from "react";
+import ClipLoader from "react-spinners/ClipLoader";
 
 function MainLayout() {
+  const { isFailure, isLoading, setHasChanged } = useContext(AppContext);
+  const [content, setContent] = useState(null);
+
+  const override = "display: block; margin: 0 auto; border-color: red;";
+
+  useEffect(() => {
+    if (isFailure) {
+      setContent(
+        <div className="error-screen">
+          <div>Loading the data from the server failed</div>
+          <button onClick={retryLoadingData}>Try Again</button>
+        </div>
+      );
+    } else if (isLoading) {
+      setContent(
+        <div className="load-screen">
+          <ClipLoader
+            color={"#ffffff"}
+            loading={isLoading}
+            css={override}
+            size={150}
+          />
+        </div>
+      );
+    } else {
+      setContent(<Outlet />);
+    }
+  }, [isLoading, isFailure]);
+
+  const retryLoadingData = () => {
+    setHasChanged(true);
+  };
+
   return (
     <div className="main-layout">
       <Sidebar />
-      <Outlet />
+      {content}
     </div>
   );
 }
